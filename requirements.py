88d80a42417d0f201422937b1dc0907716120fd2
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-06-03 14:35:39 +0800

    dwwdw

---
+++ requirements.py
@@ -0,0 +1,21 @@
+#Main Gameplay
+#1. Snake movement
+
+#2. Fruits randomly appear
+
+#3. Snake consumes fruit
+
+#4. Snake grows larger
+
+
+#Extra
+#Unlockeable Gamemode:
+    #Randomly generated walls
+
+    #Poisoned fruits
+
+    #Timer
+
+#Lore
+
+#Game Menu screen
