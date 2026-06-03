b9db2a3c710485d65fac416ff2e5633ecaa29b38
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-06-03 19:37:42 +0800

    adwdwd

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,47 @@
 import pygame
 from sys import exit #gets exit method from sys module
+from random import randint
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        new_list = []
+
+        for obstacle_type, obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_type == "rock":
+                screen.blit(rock_surface, obstacle_rect)
+            else:
+                screen.blit(drill_surface, obstacle_rect)
+            
+            print(obstacle_list)
+
+            if obstacle_rect.x > -100:
+                new_list.append((obstacle_type, obstacle_rect))
+
+        return new_list
+    else: 
+        return [] #returns an empty list so everything works
+
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacly_type, obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect):
+                return False
+    return True
+
+def animations():
+    global guy_surface, player_index
+    if guy_rect.bottom < 300:
+        guy_surface = guy_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(guy_walk):
+            player_index = 0
+        guy_surface = guy_walk[int(player_index)]
+    #walking animation
+
+    #jump
 
 pygame.init() #initializes the pygame
 screen = pygame.display.set_mode((800,400))
@@ -19,11 +61,21 @@ test_surface = pygame.Surface((1000, 100)) #creates a surface in pygame. Inside 
 test_surface.fill('White') #adds color to a defined surface
 
 rock_surface = pygame.image.load('graphics/rock.png')
-rock_rect = rock_surface.get_rect(bottomright = (700, 300))
+
+drill_surface = pygame.image.load('graphics/drill.png')
+
+
+obstacle_rect_list = []
 
 guy_surface = pygame.image.load('graphics/man1.png') #uses an image
-guy_x_pos = 0 #a variable for starting position
 
+guy_walk_1 = pygame.image.load('graphics/man1-1.png')
+guy_walk_2 = pygame.image.load('graphics/man1-2.png')
+guy_walk = [guy_walk_1, guy_walk_2]
+player_index = 0
+guy_jump = pygame.image.load('graphics/manjump.png')
+
+guy_surface = guy_walk[player_index]
 guy_rect = guy_surface.get_rect(topleft = ((0, 100))) #creates a rectangle, that you can use for a bunch of stuff
     #gets guy  makes rectangle    where do you want it? (topleft is a positional variable. Can also use midleft, midbottom. The parenthesis establishes where it is).
 
@@ -41,7 +93,7 @@ game_active = False
 
 #timer
 obstacle_timer = pygame.USEREVENT + 1 #+1 because some events are already reserved for pygame itself, the +1 resolves it
-pygame.time.set_timer(obstacle_timer, 900) #this triggers the event. Sets the event to be triggered, and how often it is to be triggered in milliseconds
+pygame.time.set_timer(obstacle_timer, 1300) #this triggers the event. Sets the event to be triggered, and how often it is to be triggered in milliseconds
 
 while True: #keeps the window open while true, otherwise it immediately closes
     for event in pygame.event.get(): #loops through all possible events in the pygame
@@ -66,12 +118,13 @@ while True: #keeps the window open while true, otherwise it immediately closes
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                rock_rect.left = 800
 
         if event.type == obstacle_timer and game_active:
-            print('test')
-                    
-        
+            if randint(0,2):
+                obstacle_rect_list.append(("rock", rock_surface.get_rect(bottomright = (randint(900,950), 300))))
+            else:
+                obstacle_rect_list.append(("drill",drill_surface.get_rect(bottomright = (randint(900,950), 200))))
+
     if game_active:
         screen.fill("black")   #makes the entire screen black, to clear the screen
         screen.blit(test_surface, (0,300)) #blit = block image transfer, fancy way of saying put a surface on another surface. The parenthesis establishes where it is located like a coordinate system
@@ -83,15 +136,20 @@ while True: #keeps the window open while true, otherwise it immediately closes
 
         #screen.blit(rock_surface, rock_rect)
 
+
+
         player_gravity += 1
         guy_rect.y += player_gravity
         if guy_rect.bottom >= 300:
             guy_rect.bottom = 300
+        animations()
         screen.blit(guy_surface, guy_rect) #uses guy_rect as a position
         #remember that pygame draws everything in the order of code, top to bottom
 
-        if rock_rect.colliderect(guy_rect):
-            game_active = False
+        #obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+        game_active = collisions(guy_rect, obstacle_rect_list)
         #draws a shape, for now a rectangle. And you need to add (the display, color, where it will be, outerwidth)
         
         pygame.draw.rect(screen,'#c0e8ec',score_rect) #so that the inside will be filled
@@ -105,6 +163,9 @@ while True: #keeps the window open while true, otherwise it immediately closes
     else:
         screen.fill('Yellow')
         screen.blit(guy_stand, guy_stand_rect)
+        obstacle_rect_list.clear()
+        guy_rect.midbottom = (80,300)
+        player_gravity = 0
 
     #if guy_rect.colliderect(rock_rect): #checks when the guy's rectangle collids with the rock's. If so, it will output a 1
         #print('collision')
