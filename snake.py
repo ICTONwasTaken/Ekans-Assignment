046d0511eb76e88af23eea23d074be7e0106a7a3
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-06-04 22:29:23 +0800

    dwdwd

--- snake.py
+++ snake.py
@@ -14,6 +14,13 @@ pygame.display.set_caption('Snake Game')
 #fps limiter
 clock = pygame.time.Clock()
 
+
+def die():
+    global highscore, collided, state
+    state = "gameover"
+    if score > highscore:
+        highscore = score
+    collided = True
 #resets the game
 def reset_game():
     #starting snake
@@ -45,18 +52,25 @@ score_font = pygame.font.Font(None, 30)
 #menu screen
 menu_surface = text_font.render('EKANS!', False, 'White')
 menu_rect = menu_surface.get_rect(center=(200, 190))
-start_surface = sub_font.render('Press Space to Begin', False, 'White')
+start_surface = sub_font.render('Press Anything to Begin', False, 'White')
 start_rect = start_surface.get_rect(center=(200, 225))
+
+#intro screen
+intro_surface = sub_font.render('There once was a snake... he had a dream of becoming large!', False, 'White')
+intro_rect = intro_surface.get_rect(center=(200, 200))
 
 #game over screen
 over_surface = text_font.render('Game Over', False, 'White')
 over_rect = over_surface.get_rect(center=(200, 190))
-retry_surface = sub_font.render('Press Space to Retry', False, 'White')
+retry_surface = sub_font.render('Press Anything to Retry', False, 'White')
 retry_rect = retry_surface.get_rect(center=(200, 225))
 
 #scoring
 score = 0
 highscore = 0
+
+default_timer = 10
+timer = default_timer
 
 score_game_surface = sub_font.render(f'Length: {score}cm', False, 'White')
 score_game_rect = score_game_surface.get_rect(topleft=(10, 10))
@@ -65,22 +79,27 @@ score_final_surface = score_font.render(f'You grew to {score}cm!', False, 'White
 score_final_rect = score_final_surface.get_rect(center=(200, 150))
 
 
-#states = "menu", "playing", "gameover"
-state = "menu"
+#states = "menu", "playing", "gameover", "intro"
+state = "intro"
 
 #animation things
 fruit_scale = 0.1
 fruit_growing = True
 flash_timer = 0
 
+direction_changed = False
+
 while True:
     for event in pygame.event.get():
-        #if player touches x, close game
+        #if player touches x, close games
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
         #if player touches escape, switch menus
+        if event.type == pygame.MOUSEBUTTONDOWN and state == "intro":
+            state = "menu"
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE and state == "menu":
                 pygame.quit()
@@ -90,6 +109,7 @@ while True:
             elif event.key == pygame.K_ESCAPE and state == "playing":
                 state = "menu"
 
+
         #basic controls
         if event.type == pygame.KEYDOWN:
             if state == "playing":
@@ -98,27 +118,38 @@ while True:
                         pygame.quit()
                         exit()
                     case pygame.K_UP:
-                        if direction != "down": #!= ensures the snake doesn't go towards itself
+                        if direction != "down" and direction_changed == False: #!= ensures the snake doesn't go towards itself
                             direction = "up"
+                            direction_changed = True
                     case pygame.K_DOWN:
-                        if direction != "up":
+                        if direction != "up" and direction_changed == False:
                             direction = "down"
+                            direction_changed = True
                     case pygame.K_RIGHT:
-                        if direction != "left":
+                        if direction != "left" and direction_changed == False:
                             direction = "right"
+                            direction_changed = True
                     case pygame.K_LEFT:
-                        if direction != "right":
+                        if direction != "right" and direction_changed == False:
                             direction = "left"
+                            direction_changed = True
+
             #play the game
-            elif state == "menu" or state == "gameover":
-                if event.key == pygame.K_SPACE:
+            else:
+                if state == "intro":
+                    state = "menu"
+                elif state == "menu" or state == "gameover":
                     snake, direction, fruit_rect = reset_game()
                     fruit_scale = 0.1
                     score = 0
                     state = "playing"
                     pygame.mouse.set_visible(False)
+                    timer = 10
+                    collided = False
+
 
     if state == "playing":
+        direction_changed = False
         #sets these two to what's inside index 0 of snake
         head_x, head_y = snake[0]
 
@@ -132,19 +163,20 @@ while True:
         elif direction == "down":
             head_y += 20
 
-        #Wall collision with buncha ORs
+        #Wall collision
         if head_x < 0 or head_x >= axis_x or head_y < 0 or head_y >= axis_y:
-            state = "gameover"
-            if score > highscore:
-                highscore = score
-            collided = True
+            die()
+            print("You hit a wall!")
+            
 
         #Self collision - checks if head x and y is the same as a part of the snake. If so, they are colliding. snake[1:] is a slice, takes everything from index 1 to the length
         elif (head_x, head_y) in snake[1:]:
             state = "gameover"
+            print("Stop hitting yourself!")
             if score > highscore:
                 highscore = score
             collided = True
+    
 
         else: #if none of the above is happening, insert a snake box at the start of the list, and remove the last
             snake.insert(0, (head_x, head_y))
@@ -162,6 +194,7 @@ while True:
                 fruit_scale = 0.1
                 fruit_growing = True
                 flash_timer = 3
+                timer = default_timer
 
             #Grow fruit scale each frame until full size
             if fruit_growing:
@@ -176,8 +209,19 @@ while True:
             #change fruit rectangle and store it in scaled fruit
             scaled_fruit = pygame.transform.rotozoom(fruit_surf, 0, fruit_scale) #surface, rotation, scale
             scaled_rect = scaled_fruit.get_rect(center=fruit_rect.center)
+        
 
             screen.fill("black") #reset screen
+
+            if score >= 5000:
+                timer -= 0.2
+                timer_surface = sub_font.render(f'Time: {timer:.1f}', False, 'White')
+                timer_rect = timer_surface.get_rect(topleft=(200, 10))
+                screen.blit(timer_surface, timer_rect)
+                if timer <= 0:
+                    state = "gameover"
+                    if score > highscore:
+                        highscore = score
 
             #create snake
             screen.blit(head_surf, head_rect)
@@ -216,6 +260,10 @@ while True:
 
         screen.blit(score_final_surface, score_final_rect)
         screen.blit(score_high_surface, score_high_rect)
+    
+    elif state == "intro":
+        screen.fill("black")
+        screen.blit(intro_surface, intro_rect)
 
     pygame.display.update()
     clock.tick(10)
